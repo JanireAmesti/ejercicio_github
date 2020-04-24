0e0be005e01f2476ec8573dad001c29676d78b47
Janire <janire.amesti@alumni.mondragon.edu>
2020-04-24 09:12:23 +0200

    Modifico programa.R

--- Carga ficheros.R
+++ Carga ficheros.R
@@ -2,3 +2,4 @@ setwd("C:/Users/Amesti/Desktop/Ejercicio-git")
 datos<-read.csv("time_series_19-covid-Confirmed.csv",encoding = "UTF-8")
 datos1<-read.csv("time_series_19-covid-Deaths.csv",encoding = "UTF-8")
 datos2<-read.csv("time_series_19-covid-Recovered.csv",encoding= "UTF-8")
+#este es el programa de carga de datos
