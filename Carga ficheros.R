0f57dc0f55e5d4fcd5bd834d18c7eb3930927dbb
Janire <janire.amesti@alumni.mondragon.edu>
2020-04-24 09:00:09 +0200

    Añado los ficheros originales

---
+++ Carga ficheros.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/Amesti/Desktop/Ejercicio-git")
+datos<-read.csv("time_series_19-covid-Confirmed.csv",encoding = "UTF-8")
+datos1<-read.csv("time_series_19-covid-Deaths.csv",encoding = "UTF-8")
+datos2<-read.csv("time_series_19-covid-Recovered.csv",encoding= "UTF-8")
